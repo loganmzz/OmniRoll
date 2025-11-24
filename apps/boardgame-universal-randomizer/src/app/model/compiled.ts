939fa4ed859cd83e9a408360f49abdaf8a6c9352
Logan Mzz <loganmzz@users.noreply.github.com>
2025-11-24 15:15:34 +0100

    feat(randomizer): Init service (#3)

--- apps/boardgame-universal-randomizer/src/app/model/compiled.ts
+++ apps/boardgame-universal-randomizer/src/app/model/compiled.ts
@@ -1,4 +1,189 @@
+import { Result } from './common';
+import { Criteria, CriteriaParser } from './criteria';
+import { DataModelComponent, DataModelGame, DataModelRandomizer, DataModelRandomizerPick, DataModelRandomizerPool, DataModelRandomizerSlot, DataModelSet } from './data-model';
+
+export class CompiledDataLocation {
+
+  constructor(readonly path: {name: string, index?:string|number}[] = []) {}
+
+  child(name: string): CompiledDataLocation {
+    return new CompiledDataLocation(this.path.concat([{name}]));
+  }
+  index(name: string, index: string|number): CompiledDataLocation {
+    return new CompiledDataLocation(this.path.concat([{name, index}]));
+  }
+
+  toString(): string {
+    return this.path
+      .map(({name,index}) => index !== undefined ? `/${name}[${index}]` : `/${name}`)
+      .join('');
+  }
+}
+
+export class CompiledDataError {
+  constructor(
+    public readonly location: CompiledDataLocation,
+    public readonly message: string,
+  ) {}
+
+  toString(): string {
+    return `${this.location}: ${this.message}`;
+  }
+}
+
 export class CompiledGame {
   key = '';
   name = '';
+  components: CompiledComponent[] = [];
+
+  static newFromDataModel(spec: DataModelGame, location: CompiledDataLocation = new CompiledDataLocation()): Result<CompiledGame, CompiledDataError[]> {
+    const compiled = new CompiledGame();
+    compiled.key = spec.key;
+    compiled.name = spec.name;
+    const errors: CompiledDataError[] = [];
+    for (const specSet of spec.sets ?? []) {
+      const result = CompiledComponent.fillFromDataModel(compiled.components, new Set(), specSet, location.index('set', specSet.key));
+      if (result.err !== undefined) {
+        errors.push(...result.err);
+      }
+    }
+    return errors.length === 0 ? Result.ok(compiled) : Result.err(errors);
+  }
+}
+
+export class CompiledComponent {
+  key = '';
+  name = '';
+  sets = new Set<string>();
+  kinds = new Set<string>();
+
+  static fillFromDataModel(output: CompiledComponent[], parentSets: Set<string>, spec: DataModelSet, location: CompiledDataLocation = new CompiledDataLocation()): Result<unknown, CompiledDataError[]> {
+    const sets = new Set(parentSets);
+    sets.add(spec.key);
+
+    const errors: CompiledDataError[] = [];
+
+    for (const childSet of spec.sets ?? []) {
+      const result = this.fillFromDataModel(output, sets, childSet);
+      if (result.err !== undefined) {
+        errors.push(...result.err);
+      }
+    }
+    for (const kind in spec.components) {
+      const specComponents = spec.components[kind];
+      for (const specComponent of specComponents) {
+        const result = this.newFromDataModel(sets, kind, specComponent);
+        if (result.ok !== undefined) {
+          if (errors.length === 0) {
+            output.push(result.ok);
+          }
+        } else {
+          errors.push(...(result.err ?? []));
+        }
+      }
+    }
+    return errors.length === 0 ? Result.ok({}) : Result.err(errors);
+  }
+
+  static newFromDataModel(parentsSets: Set<string>, kind: string, spec: DataModelComponent, location: CompiledDataLocation = new CompiledDataLocation()): Result<CompiledComponent, CompiledDataError[]> {
+    const compiled = new CompiledComponent();
+    compiled.key = spec.key;
+    compiled.name = spec.name;
+    compiled.sets = new Set(parentsSets);
+    compiled.kinds.add(kind);
+    return Result.ok(compiled);
+  }
+}
+
+export class CompiledRandomizer {
+  key = '';
+  name = '';
+  pools: CompiledRandomizerPool[] = [];
+  slots: CompiledRandomizerSlot[] = [];
+
+  static newFromDataModel(spec: DataModelRandomizer, location: CompiledDataLocation = new CompiledDataLocation()): Result<CompiledRandomizer, CompiledDataError[]> {
+    const compiled = new CompiledRandomizer();
+    compiled.key = spec.key;
+    compiled.name = spec.name;
+    const errors: CompiledDataError[] = [];
+    for (const poolSpec of spec.pools ?? []) {
+      const result = CompiledRandomizerPool.newFromDataModel(poolSpec, location.index('pool', poolSpec.key));
+      if (result.ok !== undefined) {
+        if (errors.length === 0) {
+          compiled.pools.push(result.ok);
+        }
+      } else {
+        errors.push(...(result.err ?? []));
+      }
+    }
+    for (const slotSpec of spec.slots ?? []) {
+      const result = CompiledRandomizerSlot.newFromDataModel(slotSpec, location.index('slot', slotSpec.key));
+      if (result.ok !== undefined) {
+        if (errors.length === 0) {
+          compiled.slots.push(result.ok);
+        }
+      } else {
+        errors.push(...(result.err ?? []));
+      }
+    }
+    return errors.length === 0 ? Result.ok(compiled) : Result.err(errors);
+  }
+}
+
+export class CompiledRandomizerPool {
+  key: string = '';
+  criteria: Criteria[] = [];
+
+  static newFromDataModel(spec: DataModelRandomizerPool, location: CompiledDataLocation = new CompiledDataLocation()): Result<CompiledRandomizerPool, CompiledDataError[]> {
+    const compiled = new CompiledRandomizerPool();
+    compiled.key = spec.key;
+    const specCriteriaList = spec.criteria ?? [];
+    const errors: CompiledDataError[] = [];
+    for (let i = 0; i < specCriteriaList.length; i++) {
+      const specCriteria = specCriteriaList[i];
+      const result = new CriteriaParser(specCriteria).tryReadCriteria();
+      if (result.ok !== undefined) {
+        if (errors.length === 0) {
+          compiled.criteria.push(result.ok);
+        }
+      } else {
+        errors.push(new CompiledDataError(
+          location.index('criteria', i),
+          `${result.err}`,
+        ));
+      }
+    }
+    return errors.length === 0 ? Result.ok(compiled) : Result.err(errors);
+  }
+}
+
+export class CompiledRandomizerSlot {
+  key = '';
+  pool = '';
+  pick: DataModelRandomizerPick = 'remove';
+  criteria: Criteria[] = [];
+
+  static newFromDataModel(spec: DataModelRandomizerSlot, location: CompiledDataLocation = new CompiledDataLocation()): Result<CompiledRandomizerSlot, CompiledDataError[]> {
+    const compiled = new CompiledRandomizerSlot();
+    compiled.key = spec.key;
+    compiled.pool = spec.pool;
+    compiled.pick = spec.pick ?? compiled.pick;
+    const errors: CompiledDataError[] = [];
+    const specCriteriaList = spec.criteria ?? [];
+    for (let i = 0; i < specCriteriaList.length; i++) {
+      const specCriteria = specCriteriaList[i];
+      const result = new CriteriaParser(specCriteria).tryReadCriteria();
+      if (result.ok !== undefined) {
+        if (errors.length === 0) {
+          compiled.criteria.push(result.ok);
+        }
+      } else {
+        errors.push(new CompiledDataError(
+          location.index('criteria', i),
+          `${result.err}`,
+        ));
+      }
+    }
+    return errors.length === 0 ? Result.ok(compiled) : Result.err(errors);
+  }
 }
---
+++ apps/boardgame-universal-randomizer/src/app/model/data-model.ts
@@ -0,0 +1,54 @@
+export type DataModelMergeStategy = 'merge'|'override';
+
+export type DataModelGame = {
+  key: string;
+  name: string;
+  description?: string;
+  mergeStrategy?: DataModelMergeStategy;
+  updatedAt?: string;
+  sets?: DataModelSet[];
+  randomizers?: DataModelRandomizer[];
+}
+
+export type DataModelSet = {
+  key: string;
+  name: string;
+  description?: string;
+  mergeStrategy?: DataModelMergeStategy;
+  updatedAt?: string;
+  sets?: DataModelSet[];
+  components?: Record<string, DataModelComponent[]>;
+}
+
+export type DataModelComponent = {
+  key: string;
+  name: string;
+  description?: string;
+  properties?: Record<string, boolean|number|string|string[]>;
+  mergeStrategy?: DataModelMergeStategy;
+  updatedAt?: string;
+}
+
+export type DataModelRandomizerPick = 'remove'|'keep';
+
+export type DataModelRandomizer = {
+  key: string;
+  name: string;
+  description?: string;
+  pools: DataModelRandomizerPool[];
+  slots: DataModelRandomizerSlot[];
+  mergeStrategy?: DataModelMergeStategy;
+  updatedAt?: string;
+}
+
+export type DataModelRandomizerPool = {
+  key: string;
+  criteria: string[];
+}
+
+export type DataModelRandomizerSlot = {
+  key: string;
+  pool: string;
+  pick?: DataModelRandomizerPick;
+  criteria: string[];
+}
